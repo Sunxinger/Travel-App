a77ce8bd2df84a4f70995184fb5c20b7c0c4ab9b
Sunxing Yu <yusunxing@outlook.com>
2024-04-08 00:12:47 +0100

    Location Page

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // 引入屏幕组件
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import LocationScreen from './screens/LocationScreen';
 
 // 创建底部标签导航器实例
 const Tab = createBottomTabNavigator();
@@ -18,7 +19,9 @@ export default function App() {
         {/* 屏幕定义，指定组件 */}
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Location" component={LocationScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
